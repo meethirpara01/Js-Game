309ce8e2138cdbc0a87b353e0d75affa0f3623a9
Meet Hirpara <meetkumarhirpara@gmail.com>
2025-12-17 13:08:20 +0530

    Daily backup - $(17-Dec-20205)

---
+++ script.js
@@ -0,0 +1,36 @@
+const board = document.querySelector('.board');
+const blockHeight = 50;
+const blockWidth = 50;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+const blocks = [];  
+const snake = [{x: 1, y: 3}, {x: 1, y: 4}, {x: 1, y: 5}];
+
+let direction = 'left';
+
+for (let row = 0; row < rows; row++) 
+{
+    for (let col = 0; col < cols; col++) 
+    {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        board.appendChild(block);
+        block.innerText = `${row},${col}`;
+        blocks[`${row},${col}`] = block;
+    }
+}
+
+
+function render()
+{
+    snake.forEach(segment => {
+        console.log(blocks[`${segment.x},${segment.y}`].classList.add('fill'));
+        
+    })
+}
+
+setInterval(() => {
+    render()
+}, 300);
